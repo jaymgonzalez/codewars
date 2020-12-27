9dd308c8c8a0f80993ecff71e1e95bd5f418c8f5
Jaime Munoz <jaimungon@gmail.com>
2020-12-27 18:53:35 +0100

    finish dice algorithm

--- greedDice.js
+++ greedDice.js
@@ -22,3 +22,58 @@
 // In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
 
 
+function score(dice) {
+
+  let scores = 0,
+    ones = [],
+    twos = [],
+    threes = [],
+    fours = [],
+    fives = [],
+    sixes = []
+
+
+  for (let i = 0; i < dice.length; i++) {
+    dice[i] === 1 ? ones.push(1) : false
+    dice[i] === 2 ? twos.push(1) : false
+    dice[i] === 3 ? threes.push(1) : false
+    dice[i] === 4 ? fours.push(1) : false
+    dice[i] === 5 ? fives.push(1) : false
+    dice[i] === 6 ? sixes.push(1) : false
+  }
+
+  console.log(ones, twos, threes, fours, fives, sixes)
+
+  if (ones.length > 0) {
+    ones.length === 1 ? scores += 100 : false
+    ones.length === 2 ? scores += 200 : false
+    ones.length === 3 ? scores += 1000 : false
+    ones.length === 4 ? scores += 1100 : false
+    ones.length === 5 ? scores += 1200 : false
+  }
+  if (twos.length >= 3) {
+    scores += 200
+  }
+  if (threes.length >= 3) {
+    scores += 300
+  }
+  if (fours.length >= 3) {
+    scores += 400
+  }
+  if (fives.length > 0) {
+    fives.length === 1 ? scores += 50 : false
+    fives.length === 2 ? scores += 100 : false
+    fives.length === 3 ? scores += 500 : false
+    fives.length === 4 ? scores += 550 : false
+    fives.length === 5 ? scores += 600 : false
+  }
+  if (sixes.length >= 3) {
+    scores += 600
+  }
+  console.log(scores);
+  return scores
+}
+
+score([2, 3, 4, 6, 2])
+score([4, 4, 4, 3, 3])
+score([2, 4, 4, 5, 4])
