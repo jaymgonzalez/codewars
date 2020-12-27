963053b1b7d71c78d9dd351d94415a626ab6e12b
Jaime Munoz <jaimungon@gmail.com>
2020-12-27 17:27:53 +0100

    anagrams solution

--- anagrams.js
+++ anagrams.js
@@ -19,8 +19,12 @@
 
 
 function anagrams(word, words) {
+  console.log(word)
+  console.log(words)
+
+
   const wordAsArray = word.split('')
-  let firstValue, secondValue
+  const returnedArray = []
 
   for (let i = 0; i < words.length; i++) {
     const check = words[i].split('')
@@ -29,10 +33,13 @@ function anagrams(word, words) {
 
 
     if (wordAsArray.sort().join('') == check.sort().join('')) {
-      firstValue !== undefined ? secondValue = words[i] : firstValue = words[i]
+
+      returnedArray.push(words[i])
+
+      // firstValue !== undefined ? secondValue = words[i] : firstValue = words[i]
 
 
-      // console.log(words[i])
+      console.log(words[i])
       // console.log(words[i].split('').reverse().join(''))
 
       // words[i] == words[i].reverse()
@@ -42,19 +49,24 @@ function anagrams(word, words) {
     // console.log(check)
   }
 
-  console.log(firstValue && secondValue && firstValue.split('') == secondValue.split('').reverse() ? [firstValue, secondValue] : [])
-
-  return firstValue && secondValue && firstValue.split('') == secondValue.split('').reverse() ? [firstValue, secondValue] : []
-
-
+  // console.log(firstValue && secondValue ? [firstValue, secondValue] : [])
 
   // console.log(firstValue)
   // console.log(secondValue)
+  // return firstValue && secondValue && firstValue.split('') == secondValue.split('').reverse() ? [firstValue, secondValue] : []
+
+  console.log(returnedArray);
+
+  return returnedArray
+
+
+
   // console.log(words.includes(wordAnagram))
   // console.log([wordAnagram, word]);
 
 
 }
 
-
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])
 anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+anagrams('laser', ['lazing', 'lazy', 'lacer'])
